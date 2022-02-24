f603b7a84a0661c8cf0a8538b9da89116b06e861
gaboCsharp <phoenix_kernel@hotmail.com>
2022-02-24 15:02:59 -0500

    Replace Contanier Reference in ShellVM

--- GRMDesktopUI/ViewModels/ShellViewModel.cs
+++ GRMDesktopUI/ViewModels/ShellViewModel.cs
@@ -13,18 +13,17 @@ namespace GRMDesktopUI.ViewModels
 
         private IEventAggregator _events;
         private SalesViewModel _salesVM;
-        private SimpleContainer _container;
 
         public ShellViewModel(IEventAggregator events, SalesViewModel salesVM,
             SimpleContainer contanier)
         {
             _events = events;  
             _salesVM = salesVM;
-            _container = contanier;
+
 
             _events.Subscribe(this);
 
-            ActivateItem(_container.GetInstance<LoginViewModel>());          
+            ActivateItem(IoC.Get<LoginViewModel>());          
         }
 
         public void Handle(LogOnEvent message)
